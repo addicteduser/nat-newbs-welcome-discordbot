48d77a521c9b5a3161f10a45e59033cd5c231f6a
kristine1k <kristine1k@ai-pros.com>
2020-05-18 17:58:06 +0800

    Update roll20 room link

--- bot.py
+++ bot.py
@@ -7,7 +7,7 @@ from discord.utils import get
 # from secrets import DISCORD_TOKEN
 # token = DISCORD_TOKEN
 
-# for deployment
+for deployment
 token = os.environ['DISCORD_TOKEN']
 
 
@@ -88,7 +88,7 @@ def get_welcome_message(guild, members):
         f'Welcome to the Natural Newbie server! I am `Welcome Wagon`, the server\'s semi-sentient Help AI. As such, I advise you to please head to {channel_server_guide.mention} in order to get started. You may also call my human consciousness, aka {get_human_welcome_wagon(guild).mention}, "Jed".\n\n' + \
         f'There is the {channel_tutorial.mention} for D&D-related questions that my human consciousness, the {role_admin.mention}, and the {role_dm.mention} would be happy to answer. It\'s there that you can also find the Adventurers League Players Guide (ALPG) in the pinned messages. That file contains all the information needed to create an AL-legal character. {channel_concept.mention} is the place where you can seek the wisdom and insight of the seasoned players and DMs on how to progress a specific character concept and build.\n\n' + \
         f'{channel_game_sched.mention} lists the upcoming games for the week. To discuss these, kindly head to {channel_lfg.mention}. Just above it is {channel_lfp.mention} where DMs post their scheduled sessions. Most games on this server are for characters from level 1-4 (Tier 1). On occasion, higher-tier games are run to show the newbies how more experienced players handle adventures of higher difficulty. {channel_roll20.mention} is where you may find essential information on using Roll20 properly, although you may still ask questions if you have any.\n\n' + \
-        f'Feel free to use __**The Pissing Yawa**__ (https://app.roll20.net/join/5530304/i5F18w) as a character vault. Just ask any of our ~~overlords~~, I mean, {role_admin.mention} and {role_dm.mention}, to make a folder for you and/or import your character/s from other Roll20 rooms.'
+        f'Feel free to use __**The Natural Newbie Vault**__ (https://app.roll20.net/join/7260172/yvr0fg) as a character vault. Just ask any of our ~~overlords~~, I mean, {role_admin.mention} and {role_dm.mention}, to make a folder for you and/or import your character/s from other Roll20 rooms.'
 
     return welcome_message
 
